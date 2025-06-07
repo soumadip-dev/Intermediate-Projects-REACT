64b3b2c4a343f3dc62251c959350881a09a8c156
soumadip-dev <soumadipmajila@gmail.com>
2025-06-07 12:50:52 +0530

    chore: set up Kanban board project with React, TypeScript, Vite, DnD Kit, and Chakra UI

---
+++ Kanban-Board/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button, HStack } from '@chakra-ui/react';
+const App = () => {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  );
+};
+export default App;
