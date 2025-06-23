6dfc8e6f806c1b468d8a485a11e9ece55bfcd8cf
soumadip-dev <soumadipmajila@gmail.com>
2025-06-23 12:00:00 +0530

    feat(ui): implement basic star rating component

--- usePopcorn/src/main.jsx
+++ usePopcorn/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// import App from './App.jsx'
+import StarRating from './components/StarRating';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+    <StarRating />
+  </StrictMode>
+);
